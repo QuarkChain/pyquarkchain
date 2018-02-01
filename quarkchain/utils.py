a390e6ff303b9c8209acbf626c2e92a41022bed9
Qi Zhou <qizhou@quarkchain.org>
2018-02-01 11:05:22 -0800

    add utils.py with create account feature

---
+++ quarkchain/utils.py
@@ -0,0 +1,26 @@
+import argparse
+from quarkchain.core import Identity, Address
+
+def int_left_most_bit(v):
+    """ Could be replaced by better raw implementation
+    """
+    b = 0
+    while v != 0:
+        v /= 2
+    return b
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("cmd")
+    args = parser.parse_args()
+
+    if args.cmd == "create_account":
+        iden = Identity.createRandomIdentity()
+        addr = Address.createFromIdentity(iden)
+        print("Key: %s" % iden.getKey().hex())
+        print("Account: %s" % addr.serialize().hex())
+
+
+if __name__ == '__main__':
+    main()
