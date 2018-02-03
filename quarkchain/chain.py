f310b467a81f07359c41c18d2da202031ad1325b
Qi Zhou <qizhou@quarkchain.org>
2018-02-02 21:57:33 -0800

    add RootChain

--- quarkchain/chain.py
+++ quarkchain/chain.py
@@ -1,6 +1,178 @@
 #!/usr/bin/python3
 
+from quarkchain.genesis import create_genesis_blocks
+from quarkchain.core import calculate_merkle_root, RootBlock, MinorBlock
+from ethereum import utils
+
+
+class MinorChainManager:
+    def __init__(self, env):
+        self.env = env
+        self.db = env.db
+        self.rootChain = None
+        self.blockPool = dict()  # hash to block header
+
+        tmp, self.genesisBlockList = create_genesis_blocks(env)
+
+        for mBlock in self.genesisBlockList:
+            mHash = mBlock.header.getHash()
+            self.db.put(b'mblock_' + mHash, mBlock.serialize())
+            self.blockPool[mHash] = mBlock.header
+
+    def setRootChain(self, rootChain):
+        assert(self.rootChain is None)
+        self.rootChain = rootChain
+
+    def checkValidationByHash(self, h):
+        return h in self.blockPool
+
+    def getBlockHeader(self, h):
+        return self.blockPool.get(h)
+
+    def getBlock(self, h):
+        data = self.db.get(h)
+        if data is None:
+            return None
+        return MinorBlock.deserialize(data)
+
+    def getGenesisBlock(self, shardId):
+        return self.genesisBlockList[shardId]
+
+    def addNewBlock(self, block):
+        # TODO: validate the block
+        blockData = block.serialize()
+        blockHash = utils.sha3(blockData)
+        self.blockPool[blockHash] = block.header
+        self.db.put(blockHash, blockData)
+        return True
+
 
 class RootChain:
-    def __init__(self):
+    def __init__(self, env):
+        self.env = env
+        self.db = env.db
+        self.minorChainManager = None
+        self.blockPool = dict()
+
+        # Create genesis block if not exist
+        block, tmp = create_genesis_blocks(env)
+        h = block.header.getHash()
+        if b'rblock_' + h not in self.db:
+            self.db.put(b'rblock_' + h, block.serialize())
+        self.blockPool[h] = block.header
+        self.tip = block
+        self.genesisBlock = block
+
+    def setMinorChainManager(self, manager):
+        assert(self.minorChainManager is None)
+        self.minorChainManager = manager
+
+    def loadFromDb(self):
+        # TODO
         pass
+
+    def tip(self):
+        return self.tip
+
+    def getGenesisBlock(self):
+        return self.genesisBlock
+
+    def addNewBlock(self, block):
+        """ Add new block.  The block doesn't not necessarily be appended to
+        the end of the chain.  However, if the block is the longest, then
+        will be updated
+        There are a couple of optimizations can be done here:
+        - the root block could only contain minor block header hashes as long as the shards fully validate the headers
+        - the header (or hashes) are un-ordered as long as they contains valid sub-chains from previous root block
+        """
+
+        # Check whether the block is already added
+        blockHash = block.header.getHash()
+        if blockHash in self.blockPool:
+            return True
+
+        # Check new whether previous block is in the pool
+        if block.header.hashPrevBlock not in self.blockPool:
+            return False
+        prevBlock = RootBlock.deserialize(self.db.get(
+            b'rblock_' + block.header.hashPrevBlock))
+
+        # Check the merkle tree
+        merkleHash = calculate_merkle_root(block.minorBlockHeaderList)
+        if merkleHash != block.header.hashMerkleRoot:
+            print("merkle")
+            return False
+
+        # Check whether all minor blocks are validated
+        for mheader in block.minorBlockHeaderList:
+            if not self.minorChainManager.checkValidationByHash(utils.sha3(mheader.serialize())):
+                return False
+            # Check shard size matches
+            if mheader.branch.getShardSize() != block.header.shardInfo.getShardSize():
+                return False
+
+        # Check whether all minor blocks ordered (and linked to previous block)
+        # Find the last block of previous block
+        shardId = 0
+        lastBlockHashList = []
+        for mheader in prevBlock.minorBlockHeaderList:
+            if shardId != mheader.branch.getShardId():
+                assert(shardId + 1 == mheader.branch.getShardId())
+                lastBlockHashList.append(utils.sha3(mheader))
+        lastBlockHashList.append(prevBlock.minorBlockHeaderList[-1].getHash())
+        assert(len(lastBlockHashList) == prevBlock.header.shardInfo.getShardSize())
+
+        shardId = 0
+        prevHeader = block.minorBlockHeaderList[0]
+        blockCountInShard = 1
+        if prevHeader.branch.getShardId() != 0:
+            return False
+        if prevHeader.hashPrevMinorBlock != lastBlockHashList[0]:
+            return False
+
+        totalMinorCoinbase = 0
+        for mheader in block.minorBlockHeaderList:
+            totalMinorCoinbase += mheader.coinbaseValue
+            if mheader.branch.getShardId() == shardId:
+                # Check if all minor blocks are linked in the shard
+                if mheader.hashPrevMinorBlock != utils.sha3(prevHeader):
+                    return False
+                blockCountInShard += 1
+            elif mheader.branch.getShardId() != shardId + 1:
+                # Shard id is unordered
+                return False
+            else:
+                if blockCountInShard < self.env.config.PROOF_OF_PROGRESS_BLOCKS:
+                    return False
+                # New shard is found in the list
+                shardId = mheader.branch.getShardId()
+                if mheader.hashPrevMinorBlock != lastBlockHashList[shardId]:
+                    return False
+
+            prevHeader = mheader
+        if shardId != block.shardInfo.getShardSize() - 1:
+            return False
+        if blockCountInShard < self.env.config.PROOF_OF_PROGRESS_BLOCKS:
+            return False
+
+        # Check the coinbase value is valid (we allow burning coins)
+        if mheader.coinbaseValue > totalMinorCoinbase:
+            return False
+
+        # Add the block hash to block header to memory pool and add the block to db
+        self.blockPool.put(blockHash, block.header)
+        self.db.put(b"rblock_" + blockHash, block.serialize())
+
+        if block.header.height > self.block.header.height:
+            # Switch tip
+            self.tip = block
+            # TODO switch shard tips
+        return True
+
+
+class QuarkChain:
+    def __init__(self, env):
+        self.minorChainManager = MinorChainManager(env)
+        self.rootChain = RootChain(env)
+        self.minorChainManager.setRootChain(self.rootChain)
+        self.rootChain.setMinorChainManager(self.minorChainManager)
