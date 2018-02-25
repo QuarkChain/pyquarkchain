e271fa14f4b56fa45a5fe801fdd4febab4d029af
Qi Zhou <qizhou@quarkchain.org>
2018-02-24 19:39:43 -0800

    tx_generator_with_miner submits block and generates next Txs async

--- quarkchain/testnet/tx_generator_with_miner.py
+++ quarkchain/testnet/tx_generator_with_miner.py
@@ -1,7 +1,6 @@
 import asyncio
 from quarkchain.local import OP_SER_MAP, LocalCommandOp
-from quarkchain.local import GetBlockTemplateRequest, NewTransaction
-from quarkchain.local import SubmitNewBlockRequest
+from quarkchain.local import SubmitNewBlockRequest, GetBlockTemplateRequest
 from quarkchain.protocol import Connection
 from quarkchain.config import DEFAULT_ENV
 from quarkchain.core import Transaction, TransactionInput, TransactionOutput, Code
@@ -21,6 +20,7 @@ class TxGeneratorClient(Connection):
         self.utxoPool = [[] for i in range(self.env.config.SHARD_SIZE)]
         self.miningBlock = None
         self.isMiningBlockRoot = None
+        self.utxoGenerateFuture = dict()
 
     async def start(self):
         asyncio.ensure_future(self.activeAndLoopForever())
@@ -57,91 +57,110 @@ class TxGeneratorClient(Connection):
     def mineNext(self):
         asyncio.ensure_future(self.startMining())
 
+    async def generateTx(self, shardId, prevTxList):
+        newTxList = []
+        for oldTx in prevTxList[1:]:
+            tx = Transaction(
+                inList=[TransactionInput(oldTx.getHash(), 0)],
+                code=Code.getTransferCode(),
+                outList=[oldTx.outList[0]])
+            tx.sign([self.genesisId.getKey()])
+            newTxList.append(tx)
+        self.utxoPool[shardId] = newTxList
+        future = self.utxoGenerateFuture[shardId]
+        del self.utxoGenerateFuture[shardId]
+        future.set_result(newTxList)
+
     async def startMining(self):
         print("starting mining")
-        try:
-            req = GetBlockTemplateRequest(
-                self.env.config.GENESIS_ACCOUNT, includeTx=False)
-            op, resp, rpcId = await self.writeRpcRequest(
-                LocalCommandOp.GET_BLOCK_TEMPLATE_REQUEST,
-                req)
-        except Exception as e:
-            print("Caught exception when calling GetBlockTemplateRequest {}".format(e))
-            Logger.logException()
-            self.loop.call_later(1, self.mineNext)
-            return
+        while True:
+            try:
+                req = GetBlockTemplateRequest(
+                    self.env.config.GENESIS_ACCOUNT, includeTx=False)
+                op, resp, rpcId = await self.writeRpcRequest(
+                    LocalCommandOp.GET_BLOCK_TEMPLATE_REQUEST,
+                    req)
+            except Exception as e:
+                print("Caught exception when calling GetBlockTemplateRequest {}".format(e))
+                Logger.logException()
+                await asyncio.sleep(1)
+                continue
 
-        if len(resp.blockData) == 0:
-            print(
-                "No block is available to mine, check shard mask or mine root block flag")
-            self.loop.call_later(1, self.mineNext)
-            return
+            if len(resp.blockData) == 0:
+                print(
+                    "No block is available to mine, check shard mask or mine root block flag")
+                await asyncio.sleep(1)
+                continue
 
-        if resp.isRootBlock:
-            block = RootBlock.deserialize(resp.blockData)
-            print("Starting mining on root block, height {} ...".format(
-                block.header.height))
-            # Determine whether continue to mine previous block
-            if self.miningBlock is not None and self.isMiningBlockRoot and \
-                    block.header.height == self.miningBlock.header.height:
-                block = self.miningBlock
-        else:
-            block = MinorBlock.deserialize(resp.blockData)
-            print("Starting mining on shard {}, height {} ...".format(
-                block.header.branch.getShardId(), block.header.height))
-            # Determine whether continue to mine previous block
-            if self.miningBlock is not None and not self.isMiningBlockRoot and \
-                    block.header.height == self.miningBlock.header.height and \
-                    block.header.branch == self.miningBlock.header.branch and \
-                    block.header.createTime == self.miningBlock.header.createTime:
-                block = self.miningBlock
+            if resp.isRootBlock:
+                block = RootBlock.deserialize(resp.blockData)
+                print("Starting mining on root block, height {} ...".format(
+                    block.header.height))
+                # Determine whether continue to mine previous block
+                if self.miningBlock is not None and self.isMiningBlockRoot and \
+                        block.header.height == self.miningBlock.header.height:
+                    block = self.miningBlock
             else:
-                # Fill transactions
-                block.txList.extend(self.utxoPool[block.header.branch.getShardId()])
-                block.finalizeMerkleRoot()
-
-        self.miningBlock = block
-        self.isMiningBlockRoot = resp.isRootBlock
-
-        for i in range(100000):
-            block.header.nonce += 1
-            metric = int.from_bytes(block.header.getHash(), byteorder="big") * block.header.difficulty
-            if metric < 2 ** 256:
-                print("mined on nonce {} with Txs {}".format(
-                    i, 0 if self.isMiningBlockRoot else len(block.txList)))
-                submitReq = SubmitNewBlockRequest(
-                    resp.isRootBlock, block.serialize())
-
-                try:
-                    op, submitResp, rpcId = await self.writeRpcRequest(
-                        LocalCommandOp.SUBMIT_NEW_BLOCK_REQUEST,
-                        submitReq)
-                except Exception as e:
-                    print(
-                        "Caught exception when calling SubmitNewBlockRequest {}".format(e))
-                    self.loop.call_later(1, self.mineNext)
-                    return
-
-                if submitResp.resultCode == 0:
-                    print("Mined block")
-
-                    # Generate new Txs
-                    if not self.isMiningBlockRoot:
-                        newTxList = []
-                        for oldTx in block.txList[1:]:
-                            tx = Transaction(
-                                inList=[TransactionInput(oldTx.getHash(), 0)],
-                                code=Code.getTransferCode(),
-                                outList=[oldTx.outList[0]])
-                            tx.sign([self.genesisId.getKey()])
-                            newTxList.append(tx)
-                        self.utxoPool[block.header.branch.getShardId()] = newTxList
-
+                block = MinorBlock.deserialize(resp.blockData)
+                print("Starting mining on shard {}, height {} ...".format(
+                    block.header.branch.getShardId(), block.header.height))
+                # Determine whether continue to mine previous block
+                if self.miningBlock is not None and not self.isMiningBlockRoot and \
+                        block.header.height == self.miningBlock.header.height and \
+                        block.header.branch == self.miningBlock.header.branch and \
+                        block.header.createTime == self.miningBlock.header.createTime:
+                    block = self.miningBlock
                 else:
-                    print("Mined failed, code {}, message {}".format(
-                        submitResp.resultCode, submitResp.resultMessage))
+                    if self.utxoPool[block.header.branch.getShardId()] is None:
+                        await self.utxoGenerateFuture[block.header.branch.getShardId()]
+                    # Fill transactions
+                    block.txList.extend(
+                        self.utxoPool[block.header.branch.getShardId()])
+                    block.finalizeMerkleRoot()
+
+            self.miningBlock = block
+            self.isMiningBlockRoot = resp.isRootBlock
+
+            metric = 2 ** 256
+            for i in range(100000):
+                block.header.nonce += 1
+                metric = int.from_bytes(
+                    block.header.getHash(), byteorder="big") * block.header.difficulty
+                if metric < 2 ** 256:
+                    break
+
+            if metric >= 2 ** 256:
+                # Try next block
+                continue
+
+            print("mined on nonce {} with Txs {}".format(
+                i, 0 if self.isMiningBlockRoot else len(block.txList)))
+            submitReq = SubmitNewBlockRequest(
+                resp.isRootBlock, block.serialize())
+
+            # Generate new Txs asynchronously before submitting the block
+            # Assume the following block submit will succeed
+            if not self.isMiningBlockRoot:
+                shardId = block.header.branch.getShardId()
+                self.utxoPool[shardId] = None
+                self.utxoGenerateFuture[shardId] = asyncio.Future()
+                asyncio.ensure_future(self.generateTx(shardId, block.txList[1:]))
+
+            try:
+                op, submitResp, rpcId = await self.writeRpcRequest(
+                    LocalCommandOp.SUBMIT_NEW_BLOCK_REQUEST,
+                    submitReq)
+            except Exception as e:
+                print(
+                    "Caught exception when calling SubmitNewBlockRequest {}".format(e))
+                self.close()
                 break
-        asyncio.ensure_future(self.startMining())
+
+            if submitResp.resultCode == 0:
+                print("Mined block")
+            else:
+                print("Mined failed, code {}, message {}".format(
+                    submitResp.resultCode, submitResp.resultMessage))
 
 
 def parse_args():
