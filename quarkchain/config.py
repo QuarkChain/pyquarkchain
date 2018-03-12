32ad2beab886afcf4ce958bab5c8935d05815449
wencenwu <wencen@quarkchain.org>
2018-03-12 16:54:11 -0700

    Change default shard size to 8

--- quarkchain/config.py
+++ quarkchain/config.py
@@ -14,7 +14,7 @@ class DefaultConfig:
         self.P2P_SERVER_PORT = 38291
         self.LOCAL_SERVER_PORT = 38391
         self.LOCAL_SERVER_ENABLE = False
-        self.SHARD_SIZE = 16
+        self.SHARD_SIZE = 8
         self.SHARD_SIZE_BITS = 2
 
         # Difficulty related
