e514add19b6a159d6f01c4aa012376cf80356666
qcgg <g@quarkchain.org>
2018-06-28 16:17:43 -0700

    Sleep 1 second between 100 tx batch

--- quarkchain/cluster/tx_generator.py
+++ quarkchain/cluster/tx_generator.py
@@ -72,7 +72,7 @@ class TransactionGenerator:
             if len(txList) >= 100 or total >= numTx:
                 self.slaveServer.addTxList(txList)
                 txList = []
-                await asyncio.sleep(0)  # yield CPU so that other stuff won't be held for too long
+                await asyncio.sleep(1)  # yield CPU so that other stuff won't be held for too long
 
             if total >= numTx:
                 break
