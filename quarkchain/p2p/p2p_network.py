f83b1d8e76aff9578c97fbe5290b8263e829ad17
qcdll <dll@quarkchain.org>
2018-07-14 01:26:23 -0700

    [p2p] fix a bug by prev commit

--- quarkchain/p2p/p2p_network.py
+++ quarkchain/p2p/p2p_network.py
@@ -196,6 +196,7 @@ def devp2p_app(env, network):
     # set ports based on node
     config['discovery']['listen_port'] = env.config.DEVP2P_PORT
     config['p2p']['listen_port'] = env.config.DEVP2P_PORT
+    config['p2p']['min_peers'] = min_peers
     config['p2p']['max_peers'] = max_peers
     ip = network.ip
     config['client_version_string'] = '{}:{}'.format(ip, network.port)
