a2b71eb59d9c26c54c398e1b16a0838d34856906
qcgg <g@quarkchain.org>
2018-07-23 13:56:07 -0700

    Allow generating address on specific shard

--- quarkchain/testnet/generate_accounts.py
+++ quarkchain/testnet/generate_accounts.py
@@ -1,12 +1,17 @@
 import argparse
 import json
 from quarkchain.core import Address, Identity
+from quarkchain.utils import sha3_256
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--num_accounts", default=10, type=int)
+    parser.add_argument(
+        "--shard", default=-1, type=int)
+    parser.add_argument(
+        "--shard_size", default=256, type=int)
     args = parser.parse_args()
     return args
 
@@ -14,13 +19,23 @@ def parse_args():
 def main():
     args = parse_args()
     result = []
-    for i in range(args.num_accounts):
+    while True:
         identity = Identity.createRandomIdentity()
         address = Address.createFromIdentity(identity)
+        if args.shard > -1:
+            # Follow the same algorithm in testnet web
+            fullShard = int.from_bytes(sha3_256(address.recipient.hex().encode("utf-8"))[:4], "big")
+            shard = fullShard & (args.shard_size - 1)
+            if shard != args.shard:
+                continue
+            address = Address.createFromIdentity(identity, fullShard)
         result.append({
-            "address": address.toHex(),
-            "key": identity.getKey().hex(),
+            "address": "0x" + address.toHex(),
+            "key": "0x" + identity.getKey().hex(),
         })
+        args.num_accounts -= 1
+        if args.num_accounts == 0:
+            break
 
     print(json.dumps(result, indent=4))
 
