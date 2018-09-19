cce8eb691d26cc7c3d4d752ea93b217ad69f5595
qcdll <40378568+qcdll@users.noreply.github.com>
2018-09-18 18:39:28 -0700

    get rid of CPython warning for pypy (#101)

--- setup.py
+++ setup.py
@@ -1,15 +1,30 @@
 import os
 
 from setuptools import setup
+from setuptools.command.develop import develop
 
 install_requires = set(x.strip() for x in open("requirements.txt"))
 install_requires_replacements = {}
 install_requires = [install_requires_replacements.get(r, r) for r in install_requires]
+PTH = "pypy-fix-cython-warning.pth"
 
 # Utility function to read the README file.
 # Used for the long_description.  It's nice, because now 1) we have a top level
 # README file and 2) it's easier to type in the README file than to put a raw
 # string in below ...
+
+
+class custom_develop(develop):
+    @property
+    def target(self):
+        return os.path.join(self.install_dir, PTH)
+
+    def run(self):
+        print(self.install_dir)
+        with open("quarkchain/tools/" + PTH) as infp:
+            with open(self.target, "w") as outfp:
+                outfp.write(infp.read())
+        develop.run(self)
 
 
 def read(fname):
@@ -30,4 +45,5 @@ setup(
     classifiers=["Development Status :: 0 - Development", "License :: MIT License"],
     install_requires=install_requires,
     python_requires=">=3.5",
+    cmdclass={"develop": custom_develop},
 )
