87395eb34e2f2a3a40077dd71b9e105a0ccd841b
ninjaahhh <38544039+ninjaahhh@users.noreply.github.com>
2018-10-23 11:20:20 -0700

    Apply guardian for pow check (#170)
    * Apply guardian for pow check
    
    * Change diff check and add unit test

--- quarkchain/cluster/root_state.py
+++ quarkchain/cluster/root_state.py
@@ -1,10 +1,10 @@
 import time
 import json
 import asyncio
+from typing import Optional
 
 from quarkchain.cluster.miner import validate_seal
 from quarkchain.cluster.guardian import Guardian
-from quarkchain.config import NetworkId
 from quarkchain.core import RootBlock, MinorBlockHeader, RootBlockHeader
 from quarkchain.core import (
     calculate_merkle_root,
@@ -245,22 +245,23 @@ class RootState:
         if block_hash is None:
             block_hash = header_hash
 
-        # Check difficulty
+        # Check difficulty, potentially adjusted by guardian mechanism
+        adjusted_diff = None  # type: Optional[int]
         if not self.env.quark_chain_config.SKIP_ROOT_DIFFICULTY_CHECK:
             diff = self.diff_calc.calculate_diff_with_parent(
                 prev_block_header, block_header.create_time
             )
+            if diff != block_header.difficulty:
+                raise ValueError("incorrect difficulty")
             # lower the difficulty for root block signed by guardian
             if block_header.verify_signature(
                 self.env.quark_chain_config.guardian_public_key
             ):
-                diff = Guardian.adjust_difficulty(diff, block_header.height)
-            if diff != block_header.difficulty:
-                raise ValueError("incorrect difficulty")
+                adjusted_diff = Guardian.adjust_difficulty(diff, block_header.height)
 
         # Check PoW if applicable
         consensus_type = self.env.quark_chain_config.ROOT.CONSENSUS_TYPE
-        validate_seal(block_header, consensus_type)
+        validate_seal(block_header, consensus_type, adjusted_diff=adjusted_diff)
 
         return block_hash
 
