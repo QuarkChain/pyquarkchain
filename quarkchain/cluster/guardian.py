83ee26188689524556d8fcd67ee19d3292e22e51
ninjaahhh <38544039+ninjaahhh@users.noreply.github.com>
2018-12-12 10:31:55 -0800

    Remove adaptive guardian diff adjustment (#247)

--- quarkchain/cluster/guardian.py
+++ quarkchain/cluster/guardian.py
@@ -1,11 +1,12 @@
 class Guardian:
     @staticmethod
     def adjust_difficulty(original_difficulty: int, block_height: int):
-        # TODO: decide on the parameters
-        if block_height < 1000:
-            return original_difficulty // 1000
-        if block_height < 10000:
-            return original_difficulty // 100
-        if block_height < 100000:
-            return original_difficulty // 10
-        return original_difficulty
+        return original_difficulty // 1000
+        # TODO: decide on the parameters for mainnet
+        # if block_height < 1000:
+        #     return original_difficulty // 1000
+        # if block_height < 10000:
+        #     return original_difficulty // 100
+        # if block_height < 100000:
+        #     return original_difficulty // 10
+        # return original_difficulty
