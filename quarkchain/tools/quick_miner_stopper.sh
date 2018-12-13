e93ed919147af3b522027cd0a4a562653e6540bd
Jie You <jieyou@JiedeMacBook-Pro.local>
2018-12-13 10:16:47 -0800

    kill external miner explictly

--- quarkchain/tools/quick_miner_stopper.sh
+++ quarkchain/tools/quick_miner_stopper.sh
@@ -4,6 +4,10 @@ pkill -f master.py
 
 pkill -f slave.py
 
-screen -ls | grep -i detached | cut -d. -f1 | tr -d [:blank:]| xargs kill
+pkill -f external_miner.py
+
+pkill -f check_syncing_state.py
+
+screen -ls | grep -i detached | cut -d. -f1 | tr -d [:blank:] | xargs kill
 
 wait
