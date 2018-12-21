18290441563ed63c85b21b80ab468c78e137397a
qcdll <dll@quarkchain.org>
2018-12-20 16:53:35 -0800

    multi native token id encoding

--- quarkchain/tests/test_utils.py
+++ quarkchain/tests/test_utils.py
@@ -1,4 +1,4 @@
-
+import pytest
 import random
 
 from quarkchain.core import (
@@ -9,6 +9,8 @@ from quarkchain.core import (
     Code,
     random_bytes,
 )
+
+from quarkchain.utils import token_id_encode, token_id_decode
 
 
 def create_test_transaction(
@@ -42,3 +44,23 @@ def create_random_test_transaction(from_id, to_address, amount=100, remaining=10
         random.randint(0, 100),
         random.randint(0, 100),
     )
+
+
+ENCODED_VALUES = [
+    ("0", 0),
+    ("Z", 35),
+    ("00", 36),
+    ("0Z", 71),
+    ("1Z", 107),
+    ("20", 108),
+    ("ZZ", 1331),
+    ("QKC", 35760),
+    ("ZZZZZZZZZZZZ", 4873763662273663091),
+    # ("2V4D00153RFRF", 2**64-1),
+]
+
+
+@pytest.mark.parametrize("name, id", ENCODED_VALUES)
+def test_token_id_encode(name, id):
+    assert token_id_encode(name) == id
+    assert name == token_id_decode(id)
