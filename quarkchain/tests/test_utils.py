b83535846a65ea319b3f964a98cd0d7ccde95a78
qcdll <40378568+qcdll@users.noreply.github.com>
2018-12-21 11:43:48 -0800

    multi native token id encoding (#297)
    * multi native token id encoding
    
    * more unittests and limit id to 12 chars max

--- quarkchain/tests/test_utils.py
+++ quarkchain/tests/test_utils.py
@@ -1,4 +1,4 @@
-
+import pytest
 import random
 
 from quarkchain.core import (
@@ -9,6 +9,8 @@ from quarkchain.core import (
     Code,
     random_bytes,
 )
+
+from quarkchain.utils import token_id_encode, token_id_decode, ZZZZZZZZZZZZ
 
 
 def create_test_transaction(
@@ -42,3 +44,48 @@ def create_random_test_transaction(from_id, to_address, amount=100, remaining=10
         random.randint(0, 100),
         random.randint(0, 100),
     )
+
+
+ENCODED_VALUES = [
+    ("0", 0),
+    ("Z", 35),
+    ("00", 36),
+    ("0Z", 71),
+    ("1Z", 107),
+    ("20", 108),
+    ("ZZ", 1331),
+    ("QKC", 35760),
+    ("ZZZZZZZZZZZZ", ZZZZZZZZZZZZ),
+    # ("2V4D00153RFRF", 2**64-1),
+]
+
+
+@pytest.mark.parametrize("name, id", ENCODED_VALUES)
+def test_token_id_encode(name, id):
+    assert token_id_encode(name) == id
+    assert name == token_id_decode(id)
+
+
+def test_random_token():
+    COUNT = 100000
+    random.seed(2)
+    for i in range(COUNT):
+        id = random.randint(0, ZZZZZZZZZZZZ)
+        assert id == token_id_encode(token_id_decode(id))
+
+
+def test_token_id_exceptions():
+    with pytest.raises(AssertionError):
+        token_id_encode("qkc")
+    with pytest.raises(AssertionError):
+        token_id_encode(" ")
+    with pytest.raises(AssertionError):
+        token_id_encode("btc")
+    with pytest.raises(AssertionError):
+        token_id_encode("ZZZZZZZZZZZZZ")
+    with pytest.raises(AssertionError):
+        token_id_decode(2 ** 64 - 1)
+    with pytest.raises(AssertionError):
+        token_id_decode(-1)
+    with pytest.raises(AssertionError):
+        token_id_decode(ZZZZZZZZZZZZ + 1)
