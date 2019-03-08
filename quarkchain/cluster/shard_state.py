25a416323a564314fb148ca2be46cc35626f3d3a
ninjaahhh <junjiah@protonmail.com>
2019-03-08 12:11:38 -0800

    Update shard's fork choice of root chain

--- quarkchain/cluster/shard_state.py
+++ quarkchain/cluster/shard_state.py
@@ -1311,7 +1311,7 @@ class ShardState:
             )
 
         # No change to root tip
-        if root_block.header.height <= self.root_tip.height:
+        if root_block.header.total_difficulty <= self.root_tip.total_difficulty:
             check(
                 self.__is_same_root_chain(
                     self.root_tip,
@@ -1322,7 +1322,7 @@ class ShardState:
             )
             return False
 
-        # Switch to the longest root block
+        # Switch to the root block with higher total diff
         self.root_tip = root_block.header
         self.confirmed_header_tip = shard_header
 
