e02578bd8aff7c0917de2fbb651c1aa8eab4c176
Qi Zhou <qizhou@quarkchain.org>
2019-07-02 13:53:23 -0700

    add 8 node mainnet config

--- run_cluster.sh
+++ run_cluster.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-${PYTHON:=python3} quarkchain/cluster/cluster.py --cluster_config $(realpath mainnet/singularity/cluster_config_template.json) "$@"
+${PYTHON:=python3} quarkchain/cluster/cluster.py --cluster_config $(realpath mainnet/singularity/cluster_config_template${QKC_CONFIG_EXT:=}.json) "$@"
