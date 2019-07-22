f48241b52e0082a9753156ab690abe221bcc25db
Qi Zhou <qizhou@quarkchain.org>
2019-07-22 16:42:55 -0700

    unify mk_contract_address usage (#653)
    * unify mk_contract_address usage
    
    * remove unused mk_contract_address

--- quarkchain/evm/utils.py
+++ quarkchain/evm/utils.py
@@ -144,14 +144,6 @@ def ecsign(rawhash, key):
     return v, r, s
 
 
-def mk_contract_address(sender, nonce):
-    return sha3(rlp.encode([normalize_address(sender), nonce]))[12:]
-
-
-def mk_metropolis_contract_address(sender, initcode):
-    return sha3(normalize_address(sender) + initcode)[12:]
-
-
 def safe_ord(value):
     if isinstance(value, int):
         return value
