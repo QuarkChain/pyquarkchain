91e0ffad3e3604008a37402d501c635372822ec5
Hangyu Ye <jerry.hangyu@gmail.com>
2019-08-04 01:29:26 +0800

    Update miner_address.py
    To mainnet file

--- quarkchain/tools/miner_address.py
+++ quarkchain/tools/miner_address.py
@@ -7,7 +7,7 @@ import ecdsa
 from utils import underline, colorify
 from quarkchain.utils import sha3_256
 
-FILE = "../../testnet/2.5/cluster_config_template.json"
+FILE = "../mainnet/singularity/cluster_config_template.json"
 if "QKC_CONFIG" in os.environ:
     FILE = os.environ["QKC_CONFIG"]
 
