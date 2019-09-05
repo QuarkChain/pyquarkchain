064b0f3331df506c7f2ae13269f4462c4182335e
czy1234 <chen-zhuyan@163.com>
2019-09-05 15:55:30 -0700

    Resolved issues with the highest block in old chain

--- quarkchain/cluster/shard_state.py
+++ quarkchain/cluster/shard_state.py
@@ -787,6 +787,10 @@ class ShardState:
                 break
             old_chain.append(orig_block)
             height += 1
+        if len(old_chain) > 0:
+            old_chain_highest_block = old_chain[-1]
+        else:
+            old_chain_highest_block = minor_block
 
         block = minor_block
         # Find common ancestor and record the blocks that needs to be updated
@@ -801,6 +805,9 @@ class ShardState:
                 break
             block = self.db.get_minor_block_by_hash(block.header.hash_prev_minor_block)
 
+        if len(old_chain) == 0:
+            old_chain_highest_block = None
+
         for block in old_chain:
             self.db.remove_transaction_index_from_block(block)
             self.db.remove_minor_block_index(block)
@@ -808,15 +815,20 @@ class ShardState:
                 self.__add_transactions_from_block(block)
         if len(old_chain) > 0:
             asyncio.ensure_future(
-                self.subscription_manager.notify_log(block.header, len(old_chain), True)
+                self.subscription_manager.notify_log(
+                    old_chain_highest_block, len(old_chain), True
+                )
             )
         for block in new_chain:
             self.db.put_transaction_index_from_block(block)
             self.db.put_minor_block_index(block)
             self.__remove_transactions_from_block(block)
         # new_chain has at least one block, starting from minor_block with block height descending
-        new_chain.sort(key=lambda x: x.header.height)
-        asyncio.ensure_future(self.subscription_manager.notify_new_heads(new_chain))
+        asyncio.ensure_future(
+            self.subscription_manager.notify_new_heads(
+                sorted(new_chain, key=lambda x: x.header.height)
+            )
+        )
         asyncio.ensure_future(
             self.subscription_manager.notify_log(minor_block.header, len(new_chain))
         )
