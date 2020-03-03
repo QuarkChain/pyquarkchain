86c870cfddd4ebd7a18ddf42203b9abeeb5a9f45
FreshmanQ <xunand@quarkchain.org>
2020-03-03 13:13:28 -0800

    second version

--- quarkchain/cluster/grpc_server.py
+++ quarkchain/cluster/grpc_server.py
@@ -2,14 +2,13 @@ import logging
 import argparse
 import grpc
 from quarkchain.generated import grpc_pb2, grpc_pb2_grpc
+from quarkchain.utils import Logger
 from concurrent import futures
-
-HOST = "localhost"
-PORT = 50051
 
 
 class ClusterSlave(grpc_pb2_grpc.ClusterSlaveServicer):
     def SetRootChainConfirmedBlock(self, request, context):
+        # TODO check request info to confirm blocks
         return grpc_pb2.SetRootChainConfirmedBlockResponse(
             status=grpc_pb2.ClusterSlaveStatus(code=0, message="Grpc server")
         )
@@ -18,15 +17,19 @@ class ClusterSlave(grpc_pb2_grpc.ClusterSlaveServicer):
 class GrpcServer:
     def __init__(self):
         self.server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-        grpc_pb2_grpc.add_ClusterSlaveServicer_to_server(ClusterSlave(), self.server)
-        self.add_channel(HOST, PORT)
+        self.servicer = ClusterSlave()
+        grpc_pb2_grpc.add_ClusterSlaveServicer_to_server(self.servicer, self.server)
 
     def add_channel(self, host: str, port: int):
         channel = "{}:{}".format(host, str(port))
-        self.server.add_insecure_port(channel)
+        port = self.server.add_insecure_port(channel)
 
     def start(self):
-        self.server.start()
+        try:
+            self.server.start()
+        except Exception:
+            return Logger.info("Server has been started!")
+
         self.server.wait_for_termination()
 
     def stop(self):
@@ -36,12 +39,13 @@ class GrpcServer:
 if __name__ == "__main__":
     logging.basicConfig()
     parser = argparse.ArgumentParser()
-    parser.add_argument("--host", type=str, default=HOST, help="client host")
-    parser.add_argument("--port", type=int, default=PORT, help="client port")
+    parser.add_argument("--host", type=str, default="localhost", help="client host")
+    parser.add_argument("--port", type=int, default=50051, help="client port")
 
     args = parser.parse_args()
     HOST = args.host
     PORT = args.port
 
     server = GrpcServer()
+    server.add_channel(HOST, PORT)
     server.start()
