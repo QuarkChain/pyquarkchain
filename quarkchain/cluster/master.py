62dc7f434a2662fc381d1ca4e09bb3d75c897681
lyhe18 <lyhe2018@icloud.com>
2020-03-09 09:59:28 -0700

    add call and tests to all methods of grpc client

--- quarkchain/cluster/master.py
+++ quarkchain/cluster/master.py
@@ -1293,6 +1293,7 @@ class MasterServer:
 
         for grpc_client in self.grpc_slave_pool:
             grpc_client.set_rootchain_confirmed_block()
+            grpc_client.add_root_block(r_block)
 
         result_list = await asyncio.gather(*future_list)
         check(all([resp.error_code == 0 for _, resp, _ in result_list]))
