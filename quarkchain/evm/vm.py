cc67d8c25c004370ce00ea30f40b60a22a15043b
Qi Zhou <qizhou@quarkchain.org>
2020-04-07 16:49:54 -0700

    prevent QKC from being minted (#879)
    * prevent QKC to be minted
    
    * add default_chain_token to VMExtBase

--- quarkchain/evm/vm.py
+++ quarkchain/evm/vm.py
@@ -924,4 +924,5 @@ class VmExtBase:
         self.sendmsg = lambda msg: 0, 0, 0
         self._state = state  # this field is only for mocking purpose
         self.account_exists = state.account_exists if state else None
+        self.default_chain_token = 0
         self.chain_id = 0
