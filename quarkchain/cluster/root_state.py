b7510b15b9a5f318cbe72779751fdbcd7cfe8549
ninjaahhh <junjiah@protonmail.com>
2020-04-30 19:55:55 -0700

    Fix account info posw block

--- quarkchain/cluster/root_state.py
+++ quarkchain/cluster/root_state.py
@@ -665,6 +665,4 @@ class RootState:
             block.header.hash_prev_block,
             self.db.get_root_block_header_by_hash,
         )
-        return get_posw_info(
-            config, block.header, lambda: stakes, block_cnt, signer=signer
-        )
+        return get_posw_info(config, block.header, stakes, block_cnt, signer=signer)
