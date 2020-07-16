2dbef7a9d58f95afe66cf1452ef79899d5bf7210
pichao <pichao@quarkchain.org>
2020-07-15 18:40:08 -0700

    pack balance for each shard and total balance into gauge and exposed

--- quarkchain/tools/prom.py
+++ quarkchain/tools/prom.py
@@ -1,21 +1,113 @@
-from prometheus_client import start_http_server, Summary
-import random
+import argparse
+import functools
+import logging
 import time
+from typing import List, Tuple
+from prometheus_client import start_http_server, Gauge
 
-# Create a metric to track time spent and requests made.
-REQUEST_TIME = Summary("request_processing_seconds", "Time spent processing request")
+import jsonrpcclient
+
+#
+# logging.root.setLevel(logging.INFO)
+# log_format = "%(asctime)s: %(message)s"
+# logging.basicConfig(format=log_format, datefmt="%Y-%m-%d %H:%M:%S")
+
+# disable jsonrpcclient verbose logging
+logging.getLogger("jsonrpcclient.client.request").setLevel(logging.WARNING)
+logging.getLogger("jsonrpcclient.client.response").setLevel(logging.WARNING)
+
+TIMEOUT = 10
+
+host = "http://localhost:38391"
 
 
-# Decorate function with metric.
-@REQUEST_TIME.time()
-def process_request(t):
-    """A dummy function that takes some time."""
-    time.sleep(t)
+@functools.lru_cache(maxsize=5)
+def get_jsonrpc_cli(jrpc_url):
+    return jsonrpcclient.HTTPClient(jrpc_url)
+
+
+def get_latest_minor_block_id_from_root_block(root_block_height: int) -> List[str]:
+    global host
+    cli = get_jsonrpc_cli(host)
+    res = cli.send(
+        jsonrpcclient.Request("getRootBlockByHeight", hex(root_block_height)),
+        timeout=TIMEOUT,
+    )
+    if not res:
+        raise RuntimeError("Failed to query root block at height" % root_block_height)
+
+    # Chain ID + shard ID uniquely determines a shard.
+    shard_to_header = {}
+    for mh in res["minorBlockHeaders"]:
+        # Assumes minor blocks are sorted by shard and height.
+        shard_to_header[mh["chainId"] + mh["shardId"]] = mh["id"]
+
+    return list(shard_to_header.values())
+
+
+def count_total_balance(block_id: str, token_id: int, starter: str) -> Tuple[int, str]:
+    global host
+    cli = get_jsonrpc_cli(host)
+    res = cli.send(
+        jsonrpcclient.Request("getTotalBalance", block_id, hex(token_id), starter),
+        timeout=TIMEOUT,
+    )
+    if not res:
+        raise RuntimeError("Failed to count total balance")
+    return int(res["totalBalance"], 16), res["next"]
+
+
+def get_balance(root_block_height, token_id):
+    # TODO: handle cases if the root block doesn't contain all the shards.
+    minor_block_ids = get_latest_minor_block_id_from_root_block(root_block_height)
+
+    total_balances = {}
+    for block_id in minor_block_ids:
+        shard = "0x" + block_id[-8:]
+        total, starter, cnt = 0, None, 0
+        while starter != "0x" + "0" * 40:
+            balance, starter = count_total_balance(block_id, token_id, starter)
+            total += balance
+            cnt += 1
+        total_balances[shard] = total / 1e18
+    return total_balances
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--rheight", type=int, help="root block height to query", required=True
+    )
+    parser.add_argument("--token", type=str, help="token ID to query", default="0x8bb0")
+    parser.add_argument("--host", type=str, help="host address of the cluster")
+    args = parser.parse_args()
+
+    global host
+    if args.host:
+        host = args.host
+        # Assumes http by default.
+        if not host.startswith("http"):
+            host = "http://" + host
+
+    token_id = int(args.token, 16)
+    root_block_height = args.rheight
+
+    start_http_server(8000)
+    # Create a metric to track qkc total balance
+    QKC_TOTAL_BALANCE = Gauge("qkc_total_balance", "Total balance in current net")
+    # Use a dict to store gauge for each shard
+    QKC_SHARD_BALANCE = {}
+    while True:
+        total_balance = get_balance(root_block_height, token_id)
+        QKC_TOTAL_BALANCE.set(sum(total_balance.values()))
+        for shard, bal in total_balance.items():
+            if shard not in QKC_SHARD_BALANCE:
+                QKC_SHARD_BALANCE[shard] = Gauge(
+                    f"qkc_shard_{shard}_balance", f"Total balance in shard {shard}"
+                )
+            QKC_SHARD_BALANCE[shard].set(bal)
+        time.sleep(60)
 
 
 if __name__ == "__main__":
-    # Start up the server to expose the metrics.
-    start_http_server(8000)
-    # Generate some requests.
-    while True:
-        process_request(random.random())
+    main()
