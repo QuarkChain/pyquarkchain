da33e8c0daeea7278d60f7c39ab1c29a1d298b76
pingke <pingke@quarkchain.org>
2021-06-04 18:56:08 +0800

    change jsonrpc to support eip155 signer

--- quarkchain/cluster/jsonrpc.py
+++ quarkchain/cluster/jsonrpc.py
@@ -87,8 +87,7 @@ def data_decoder(hex_str, allow_optional=False):
 
 
 def data_encoder(data_bytes):
-    """Encode unformatted binary `dataBytes`.
-    """
+    """Encode unformatted binary `dataBytes`."""
     return "0x" + data_bytes.hex()
 
 
@@ -132,12 +131,12 @@ def full_shard_key_encoder(full_shard_key):
 
 
 def id_encoder(hash_bytes, full_shard_key):
-    """ Encode hash and full_shard_key into hex """
+    """Encode hash and full_shard_key into hex"""
     return data_encoder(hash_bytes + full_shard_key.to_bytes(4, byteorder="big"))
 
 
 def id_decoder(hex_str):
-    """ Decode an id to (hash, full_shard_key) """
+    """Decode an id to (hash, full_shard_key)"""
     data_bytes = data_decoder(hex_str)
     if len(data_bytes) != 36:
         raise InvalidParams("Invalid id encoding")
@@ -204,7 +203,9 @@ def root_block_encoder(block, extra_info):
     return d
 
 
-def minor_block_encoder(block, include_transactions=False, extra_info=None):
+def minor_block_encoder(
+    quark_chain_config, block, include_transactions=False, extra_info=None
+):
     """Encode a block as JSON object.
 
     :param block: a :class:`ethereum.block.Block`
@@ -227,7 +228,7 @@ def minor_block_encoder(block, include_transactions=False, extra_info=None):
     if include_transactions:
         d["transactions"] = []
         for i, _ in enumerate(block.tx_list):
-            d["transactions"].append(tx_encoder(block, i))
+            d["transactions"].append(tx_encoder(quark_chain_config, block, i))
     else:
         d["transactions"] = [
             id_encoder(tx.get_hash(), block.header.branch.get_full_shard_id())
@@ -262,13 +263,14 @@ def minor_block_header_encoder(header: MinorBlockHeader) -> Dict:
     return d
 
 
-def tx_encoder(block, i):
+def tx_encoder(quark_chain_config, block, i):
     """Encode a transaction as JSON object.
 
     `transaction` is the `i`th transaction in `block`.
     """
     tx = block.tx_list[i]
     evm_tx = tx.tx.to_evm_tx()
+    evm_tx.set_quark_chain_config(quark_chain_config)
     branch = block.header.branch
     return {
         "id": id_encoder(tx.get_hash(), evm_tx.from_full_shard_key),
@@ -814,7 +816,9 @@ class JSONRPCHttpServer:
         )
         if not block:
             return None
-        return minor_block_encoder(block, include_transactions, extra_info)
+        return minor_block_encoder(
+            self.master.env.quark_chain_config, block, include_transactions, extra_info
+        )
 
     @public_methods.add
     @decode_arg("full_shard_key", quantity_decoder)
@@ -842,7 +846,9 @@ class JSONRPCHttpServer:
         )
         if not block:
             return None
-        return minor_block_encoder(block, include_transactions, extra_info)
+        return minor_block_encoder(
+            self.master.env.quark_chain_config, block, include_transactions, extra_info
+        )
 
     @public_methods.add
     @decode_arg("tx_id", id_decoder)
@@ -858,7 +864,7 @@ class JSONRPCHttpServer:
             return None
         if len(minor_block.tx_list) <= i:
             return None
-        return tx_encoder(minor_block, i)
+        return tx_encoder(self.master.env.quark_chain_config, minor_block, i)
 
     @public_methods.add
     @decode_arg("block_height", block_height_decoder)
@@ -925,7 +931,7 @@ class JSONRPCHttpServer:
     @decode_arg("limit", quantity_decoder)
     async def getAllTransactions(self, full_shard_key, start="0x", limit="0xa"):
         """ "start" should be the "next" in the response for fetching next page.
-            "start" can also be "0x" to fetch from the beginning (i.e., latest).
+        "start" can also be "0x" to fetch from the beginning (i.e., latest).
         """
         branch = Branch(
             self.master.env.quark_chain_config.get_full_shard_id_by_full_shard_key(
@@ -952,8 +958,8 @@ class JSONRPCHttpServer:
         self, address, start="0x", limit="0xa", transfer_token_id=None
     ):
         """ "start" should be the "next" in the response for fetching next page.
-            "start" can also be "0x" to fetch from the beginning (i.e., latest).
-            "start" can be "0x00" to fetch the pending outgoing transactions.
+        "start" can also be "0x" to fetch from the beginning (i.e., latest).
+        "start" can be "0x00" to fetch the pending outgoing transactions.
         """
         address = Address.create_from(address)
         if limit > 20:
@@ -1052,9 +1058,13 @@ class JSONRPCHttpServer:
         minor_block, i = await self.master.get_transaction_by_hash(tx_hash, branch)
         if not minor_block:
             return None
-        confirming_hash = self.master.root_state.db.get_root_block_confirming_minor_block(
-            minor_block.header.get_hash()
-            + minor_block.header.branch.get_full_shard_id().to_bytes(4, byteorder="big")
+        confirming_hash = (
+            self.master.root_state.db.get_root_block_confirming_minor_block(
+                minor_block.header.get_hash()
+                + minor_block.header.branch.get_full_shard_id().to_bytes(
+                    4, byteorder="big"
+                )
+            )
         )
         if confirming_hash is None:
             return quantity_encoder(0)
@@ -1109,7 +1119,9 @@ class JSONRPCHttpServer:
         )
         if block is None:
             return None
-        return block_transcoder(minor_block_encoder(block))
+        return block_transcoder(
+            minor_block_encoder(self.master.env.quark_chain_config, block)
+        )
 
     @public_methods.add
     @decode_arg("address", eth_address_to_quarkchain_address_decoder)
@@ -1149,7 +1161,7 @@ class JSONRPCHttpServer:
     @public_methods.add
     @decode_arg("shard", shard_id_decoder)
     async def eth_call(self, data, shard=None):
-        """ Returns the result of the transaction application without putting in block chain """
+        """Returns the result of the transaction application without putting in block chain"""
         data = self._convert_eth_call_data(data, shard)
         return await self.call(data)
 
@@ -1289,8 +1301,10 @@ class JSONRPCHttpServer:
         if limit > 10000:
             limit = 10000
         block_hash, full_shard_key = block_id
-        full_shard_id = self.master.env.quark_chain_config.get_full_shard_id_by_full_shard_key(
-            full_shard_key
+        full_shard_id = (
+            self.master.env.quark_chain_config.get_full_shard_id_by_full_shard_key(
+                full_shard_key
+            )
         )
         try:
             result = await self.master.get_total_balance(
@@ -1324,7 +1338,7 @@ class JSONRPCHttpServer:
 
     @private_methods.add
     async def getKadRoutingTable(self):
-        """ returns a list of nodes in the p2p discovery routing table, in the enode format
+        """returns a list of nodes in the p2p discovery routing table, in the enode format
         eg. "enode://PUBKEY@IP:PORT"
         """
         if not isinstance(self.master.network, P2PManager):
@@ -1373,7 +1387,7 @@ class JSONRPCHttpServer:
         return loglist_encoder(logs)
 
     async def _call_or_estimate_gas(self, is_call: bool, **data):
-        """ Returns the result of the transaction application without putting in block chain """
+        """Returns the result of the transaction application without putting in block chain"""
         if not isinstance(data, dict):
             raise InvalidParams("Transaction must be an object")
 
