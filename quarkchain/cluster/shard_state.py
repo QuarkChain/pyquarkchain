0bed0a77e6bbfc4dafe10f906fbef7e0f20bb8a4
pingke <pingke@quarkchain.org>
2021-06-11 14:41:51 +0800

    fix test

--- quarkchain/cluster/shard_state.py
+++ quarkchain/cluster/shard_state.py
@@ -450,11 +450,12 @@ class ShardState:
         evm_tx.set_quark_chain_config(self.env.quark_chain_config)
 
         if evm_tx.network_id != self.env.quark_chain_config.NETWORK_ID:
-            raise RuntimeError(
-                "evm tx network id mismatch. expect {} but got {}".format(
-                    self.env.quark_chain_config.NETWORK_ID, evm_tx.network_id
+            if evm_tx.version != 2:
+                raise RuntimeError(
+                    "evm tx network id mismatch. expect {} but got {}".format(
+                        self.env.quark_chain_config.NETWORK_ID, evm_tx.network_id
+                    )
                 )
-            )
 
         if not self.branch.is_in_branch(evm_tx.from_full_shard_key):
             raise RuntimeError(
