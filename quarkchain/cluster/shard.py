6830a3ce4b6739685a44e3e8fd32b01c2bcb2751
Qi Zhou <qzhou64@gmail.com>
2022-04-16 03:38:32 -0700

    skip shard sync from peer tip if its root block has fork

--- quarkchain/cluster/shard.py
+++ quarkchain/cluster/shard.py
@@ -245,6 +245,17 @@ class PeerShardConnection(VirtualConnection):
         if self.shard_state.header_tip.height >= m_header.height:
             return
 
+        # Do not download if the prev root block is not synced
+        rblock_header = self.state.get_root_block_header_by_hash(m_header.hash_prev_root_block)
+        if (rblock_header is None):
+            return
+
+        # Do not download if the new header's confirmed root is lower then current root tip last header's confirmed root
+        # This means the minor block's root is a fork, which will be handled by master sync
+        confirmed_root_header = self.state.get_root_block_header_by_hash(self.shard_state.confirmed_header_tip.hash_prev_root_block)
+        if confirmed_root_header != None and confirmed_root_header.height > rblock_header.height:
+            return
+
         Logger.info_every_sec(
             "[{}] received new tip with height {}".format(
                 m_header.branch.to_str(), m_header.height
