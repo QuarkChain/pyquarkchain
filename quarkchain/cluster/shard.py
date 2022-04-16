b13321785df8018068c0da2c31c9d407685fa885
Qi Zhou <qzhou64@gmail.com>
2022-04-16 10:44:01 -0700

    fix

--- quarkchain/cluster/shard.py
+++ quarkchain/cluster/shard.py
@@ -695,7 +695,7 @@ class Shard:
 
         # Do not download if the new header's confirmed root is lower then current root tip last header's confirmed root
         # This means the minor block's root is a fork, which will be handled by master sync
-        confirmed_root_header = self.shard_state.get_root_block_header_by_hash(self.shard_state.confirmed_header_tip.hash_prev_root_block)
+        confirmed_root_header = self.state.get_root_block_header_by_hash(self.state.confirmed_header_tip.hash_prev_root_block)
         if confirmed_root_header is not None and confirmed_root_header.height > rblock_header.height:
             return
 
