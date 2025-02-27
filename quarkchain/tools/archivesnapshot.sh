41ef8175882fd3be9ad553739a905b6a4ca6244f
ping-ke <pingke@quarkchain.org>
2025-02-27 14:08:45 +0800

    Update nightly-check-db actions: download quarkchain snapshot from storage box instead of S3 (#957)
    * update action checkout version

---
+++ quarkchain/tools/archivesnapshot.sh
@@ -0,0 +1,22 @@
+sshpass -p $PriKey scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null u446960@u446960.your-storagebox.de:public-data/VERSION VERSION
+
+filename=`cat VERSION`
+
+if [ $filename = "Archive-A.tar.gz" ]; then
+  filename="Archive-B.tar.gz"
+else
+  filename="Archive-A.tar.gz"
+fi
+echo $filename
+
+
+rm $filename
+curl https://s3-us-west-2.amazonaws.com/qkcmainnet/data/`curl https://s3-us-west-2.amazonaws.com/qkcmainnet/data/LATEST`.tar.gz --output $filename
+
+sshpass -p $PriKey scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $filename u446960@u446960.your-storagebox.de:public-data/$filename
+
+echo $filename > VERSION
+
+sshpass -p $PriKey scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null VERSION u446960@u446960.your-storagebox.de:public-data/VERSION
+
+rm $filename
