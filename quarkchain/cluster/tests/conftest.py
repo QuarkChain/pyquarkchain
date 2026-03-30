41b84caf82e4753d09609532fede389e1d33dc1a
ping-ke <molaokp@gmail.com>
2026-03-30 17:38:00 +0800

    remove useless code

--- quarkchain/cluster/tests/conftest.py
+++ quarkchain/cluster/tests/conftest.py
@@ -15,7 +15,6 @@ def ensure_event_loop():
     try:
         old_loop = asyncio.get_event_loop()
         if old_loop.is_closed():
-            old_loop.close()
             asyncio.set_event_loop(asyncio.new_event_loop())
     except RuntimeError:
         asyncio.set_event_loop(asyncio.new_event_loop())
